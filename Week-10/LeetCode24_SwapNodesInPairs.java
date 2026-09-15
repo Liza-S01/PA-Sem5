de1c2127fa10cda4f0e460363549c2332841a977
Liza-S01 <singlaliza0107@gmail.com>
2026-09-15 17:12:18 +0530

    added LeetCode24_SwapNodesInPairs

---
+++ Week-10/LeetCode24_SwapNodesInPairs.java
@@ -0,0 +1,31 @@
+class Main {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while (prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+}
