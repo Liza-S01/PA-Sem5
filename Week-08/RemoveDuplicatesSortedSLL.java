dde1993c5a3f1ab0513f7cd5482d834795e2596e
Liza-S01 <singlaliza0107@gmail.com>
2026-09-15 17:12:15 +0530

    added RemoveDuplicatesSortedSLL

---
+++ Week-08/RemoveDuplicatesSortedSLL.java
@@ -0,0 +1,56 @@
+public class RemoveDuplicatesSortedSLL {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+
+                curr.next = curr.next.next;
+            } else {
+
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode curr = head;
+        while (curr != null) {
+            System.out.print(curr.val + (curr.next != null ? " -> " : ""));
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        head.next.next.next.next.next = new ListNode(3);
+        head.next.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next.next.next = new ListNode(5);
+
+        System.out.print("Before removing duplicates: ");
+        printList(head);
+
+        head = deleteDuplicates(head);
+
+        System.out.print("After removing duplicates:  ");
+        printList(head);
+    }
+}
