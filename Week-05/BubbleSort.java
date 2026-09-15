e6e39607bf269bfe26552cab5df178f0438903bf
Liza-S01 <singlaliza0107@gmail.com>
2026-09-15 17:12:10 +0530

    added BubbleSort

---
+++ Week-05/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+        System.out.println("Original Array: " + Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array (Bubble Sort): " + Arrays.toString(arr));
+    }
+}
